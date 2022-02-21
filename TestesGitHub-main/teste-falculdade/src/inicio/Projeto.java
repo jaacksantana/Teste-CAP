021239e65fbed37d80c732527a798264ac092f0c
jaacksantana <98662751+jaacksantana@users.noreply.github.com>
2022-02-21 00:23:07 -0300

    Update Projeto.java

--- TestesGitHub-main/teste-falculdade/src/inicio/Projeto.java
+++ TestesGitHub-main/teste-falculdade/src/inicio/Projeto.java
@@ -5,6 +5,8 @@ public class Projeto {
 	public static void main(String[] args) {
 
 		Teste01 teste1 = new Teste01();
+		teste1.setValor(10);
+		teste1.RealizarTeste();
 
 		Teste02 teste2 = new Teste02();
 		teste2.setSenha("aAa3a@ab");
